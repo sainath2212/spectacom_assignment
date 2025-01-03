89288ce758e8e10e6b5e15ce540d12d32c6ebc71
sainath2212 <151456271+sainath2212@users.noreply.github.com>
2025-01-03 18:03:13 +0530

    Update Contact.jsx

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,17 +1,21 @@
 function Contact() {
   return (
-    <div className="bg-[#FAFAFA] py-20 px-20">
-      <h1 className="text-center">Stay upto date, join our newsletter</h1>
-      <div className="flex justify-center items-center gap-2 mt-5 flex-wrap">
+    <div className="bg-[#FAFAFA] py-10 px-5 sm:py-20 sm:px-20">
+      <h1 className="text-center text-[20px] sm:text-[28px] font-semibold">
+        Stay up to date, join our newsletter
+      </h1>
+      <div className="flex flex-col sm:flex-row justify-center items-center gap-5 mt-5">
         <input
-          className="w-full sm:w-1/2 p-5 border border-[#CCCCCC] rounded-md"
+          className="w-full sm:w-1/2 p-3 sm:p-5 border border-[#CCCCCC] rounded-md"
           placeholder="ENTER YOUR EMAIL"
-        ></input>
-        <button className="bg-[#b89840] rounded-md py-5 px-10 text-white mt-5 sm:mt-0 sm:ml-5">
+          type="email"
+        />
+        <button className="bg-[#b89840] w-full sm:w-auto rounded-md py-3 sm:py-5 px-10 text-white font-medium">
           LET'S TALK
         </button>
       </div>
     </div>
   );
 }
+
 export default Contact;
